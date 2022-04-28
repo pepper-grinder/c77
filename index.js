64c37ec5a90952d6c8488ad5a97632e72c6f83a9
pepper-grinder <94027236+pepper-grinder@users.noreply.github.com>
2022-04-27 20:51:05 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var firstname = "dhruv ";
+var lastname = "adhikari";
+console.log(firstname.concat(lastname));
+
+var e = 7;
+var f = 3;
+console.log();
+console.log(Math.pow(e,f));
+console.log(e*f-f);
+console.log(Math.round(Math.pow(e,e)/f));
+console.log(Math.round(Math.random()*Math.pow(e,f)));
